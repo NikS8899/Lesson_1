33c278bfd7c150f6fbd194a3d43e9fd88448167a
NikitaS <sergio791309@gmail.com>
2022-07-12 21:19:15 +0300

    Программа для задачи 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+Console.WriteLine("Все четные числа от 1 до N: ");
+while (number < n)
+{
+number ++;
+if (number %2 == 0)
+{
+    Console.WriteLine(number);
+}
+
+}
+
