a281bfd733455510b693566b1879419c9b85fc3d
NikitaS <sergio791309@gmail.com>
2022-07-12 20:54:51 +0300

    Программа для задачи 3

---
+++ Задача3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Проверка четности числа, введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n %2 == 0)
+{
+Console.WriteLine("Число является четным!");
+}
+else
+{
+    Console.WriteLine("Число НЕ является четным!");
+}
