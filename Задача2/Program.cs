33d9fe7671090f09e205c28eaf9efcb05da4a9f9
NikitaS <sergio791309@gmail.com>
2022-07-12 20:46:07 +0300

    Программа для задачи 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>max)
+{
+    max = a;
+}
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b>max)
+{
+    max = b;
+}
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (c>max)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное число: " + max);
